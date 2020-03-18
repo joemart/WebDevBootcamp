1baad98a9a252b07b367f735022ffba3ee8a00fa
Joseph Martire <joemart06@gmail.com>
2020-03-18 15:28:27 -0400

    added registering to v6

--- YelpCamp/v6/Routes/register.js
+++ YelpCamp/v6/Routes/register.js
@@ -1,5 +1,6 @@
 let route = require("express").Router()
 
+
 route.get("/", async (req,res)=>{
 
 	res.render("signup/register")
@@ -9,12 +10,14 @@ route.post("/", async (req,res)=>{
 	try{
 		let {username, password} = req.body;
 		await User.register(new User({username:username}), password)
-		passport.authenticate("local")
+		passport.authenticate("local")(req,res,()=>{
+			res.redirect("/campgrounds")
+		})
 	}
 	catch(e)
 	{
 		console.log(e)
-		res.render("register")
+		res.render("./signup/register")
 	}
 })
 
