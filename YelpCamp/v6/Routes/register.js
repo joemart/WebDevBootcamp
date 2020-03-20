193bc852595da8524fdf6d22dc68f1f2da286552
Joseph Martire <joemart06@gmail.com>
2020-03-20 19:50:21 -0400

    register done right

--- YelpCamp/v6/Routes/register.js
+++ YelpCamp/v6/Routes/register.js
@@ -1,18 +1,29 @@
 let route = require("express").Router()
-
-
+let User 		= require('../models/User')
+let passport 	= require("passport")
+ 
 route.get("/", async (req,res)=>{
-
+ 
 	res.render("signup/register")
 })
-
-route.post("/", async (req,res)=>{
+ 
+route.post("/", async (req,res,next)=>{
 	try{
 		let {username, password} = req.body;
-		await User.register(new User({username:username}), password)
-		passport.authenticate("local")(req,res,()=>{
-			res.redirect("/campgrounds")
-		})
+ 
+ 
+		await User.register(new User({username}), password)
+		passport.authenticate("local", (err, user, info)=>{ //memorize this function
+			
+			if(err) return next(err)
+							
+			if(!user) return res.send("bad")
+ 
+			req.logIn(user, (err)=>{
+				if(err) return next(err)
+					return res.redirect("/campgrounds")
+			})
+		})(req,res,next)
 	}
 	catch(e)
 	{
@@ -20,5 +31,5 @@ route.post("/", async (req,res)=>{
 		res.render("./signup/register")
 	}
 })
-
+ 
 module.exports = route
